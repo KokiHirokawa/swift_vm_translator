5b3f8809f8ddaaa93e0ecf03981303863f18a1d5
KokiHirokawa <kokihirokawa1110@icloud.com>
2019-03-22 17:13:38 +0900

    Initial Commit

---
+++ swift-vm-translator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-vm-translator
+//
+//  Created by KokiHirokawa on 2019/03/22.
+//  Copyright © 2019 KokiHirokawa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
